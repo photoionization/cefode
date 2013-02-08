070dfe8e3450a9d74df1c5575de7d3631e57f6ba
Cheng Zhao <zcbenz@gmail.com>
2013-02-08 14:47:39 +0000

    Add simple tests.

---
+++ test/tests/require/require.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+
+describe('require', function() {
+  describe('behaviour', function() {
+    it('type is function', function() {
+      assert.equal(typeof global.require, 'function');
+    });
+  });
+
+  describe('members', function() {
+    it('have resolve', function() {
+      assert.equal(typeof global.require.resolve, 'function');
+    });
+
+    it('have main', function() {
+      assert.equal(typeof global.require.main, 'object');
+    });
+
+    it('have extensions', function() {
+      assert.equal(typeof global.require.extensions, 'object');
+    });
+
+    it('have registerExtension', function() {
+      assert.equal(typeof global.require.registerExtension, 'function');
+    });
+
+    it('have cache', function() {
+      assert.equal(typeof global.require.cache, 'object');
+    });
+  });
+});
+
+describe('crashes', function() {
+  describe('context', function() {
+    it('process.nextTick', function(done) {
+      process.nextTick(done);
+    });
+  });
+});
+
