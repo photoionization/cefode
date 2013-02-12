00e7fa6354c1247cfa321354df90b624860ec4a1
Cheng Zhao <zcbenz@gmail.com>
2013-02-12 14:59:02 +0000

    Add test for DOM's simple WebWorker.

--- test/tests/dom/dom.js
+++ test/tests/dom/dom.js
@@ -9,6 +9,16 @@ describe('dom', function() {
       }
       mc.port2.postMessage("HELLO");
     });
+
+    it('WebWorker', function(done) {
+      var worker = new Worker('tests/dom/worker.js');
+      worker.addEventListener('message', function(e) {
+        assert.equal(e.data, 'Hello World!');
+        done();
+      }, false);
+
+      worker.postMessage('Hello World!');
+    });
   });
 });
 
